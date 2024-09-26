4f9270786f51dc6872b19094169397ee965cd3aa
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-26 15:50:58 +0330

    login view and reverse page was designed

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from core import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'core'
 
 urlpatterns = [
-    
+    path('login/', views.CustomLoginView.as_view(), name="login"),
+    path('dashboard/', login_required(views.Dashboard.as_view()), name='dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.views import View
+from core.forms import LoginForm
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'core/login.html'
+    redirect_authenticated_user = True
+    login_url = '/login/'
+    form_class = LoginForm
+    
+    def get_success_url(self):
+        return reverse_lazy('core:dashboard')
+    
+
+class Dashboard(View):
+    
+    def get(self,request):
+        return render(request, 'core/dashboard.html')
