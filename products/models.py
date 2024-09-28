52f4303085597b5fc668458012bd4770299b87e3
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-28 14:25:53 +0330

    product model was defined

--- products/models.py
+++ products/models.py
@@ -7,3 +7,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', default='images/default.png')
+
+    def __str__(self):
+        return self.name
