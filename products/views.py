2ad89b0ac9c70e5648e01c3cfc9d86f2e0d91359
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-28 14:30:34 +0330

    submiting product form view was added

--- products/views.py
+++ products/views.py
@@ -1,31 +1,47 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from products.models import Category
-from products.forms import CategoryForm
+from products.forms import CategoryForm, ProductForm
 
 
 class CreateCategory(View):
     def get(self, request, *args, **kwargs):
-        form = CategoryForm()
+        category_form = CategoryForm()
+        product_form = ProductForm()
         categories = Category.objects.all()
         context = {
-            'form': form,
+            'category_form': category_form,
             'categories': categories,
+            'product_form': product_form,
         }
         return render(request, 'core/dashboard.html', context)
     
     def post(self, request):
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('products:dashboard')
-        categories = Category.objects.all()
-        context = {
-            'form': form,
-            'categories': categories,
-        }
-        return render(request, 'core/dashboard.html', context)
-    
+        if request.POST.get('create') == 'category':
+            form = CategoryForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('products:dashboard')
+            categories = Category.objects.all()
+            context = {
+                'form': form,
+                'categories': categories,
+            }
+            return render(request, 'core/dashboard.html', context)
+
+        elif request.POST.get('create') == 'product':
+            print('create product')
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return redirect('products:dashboard')
+            categories = Category.objects.all()
+            context = {
+                'form': form,
+                'categories': categories,
+            }
+            return render(request, 'core/dashboard.html', context)
+
 
 class DeleteCategory(View):
     def post(self, request, cat_id):
