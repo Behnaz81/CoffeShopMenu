915c7c17b025f06b60b879e3ee7ca17d20b7f3a4
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-28 11:17:58 +0330

    delete category view was provided

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('dashboard/', login_required(views.CreateCategory.as_view()), name='dashboard'),
+    path('delete-cat/<int:cat_id>/', login_required(views.DeleteCategory.as_view()), name='delete-cat'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from products.models import Category
 from products.forms import CategoryForm
 
 
 class CreateCategory(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         form = CategoryForm()
         categories = Category.objects.all()
         context = {
@@ -25,3 +25,10 @@ class CreateCategory(View):
             'categories': categories,
         }
         return render(request, 'core/dashboard.html', context)
+    
+
+class DeleteCategory(View):
+    def post(self, request, cat_id):
+        category = get_object_or_404(Category, id=cat_id)
+        category.delete()
+        return redirect('products:dashboard')
