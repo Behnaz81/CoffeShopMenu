76316879ad6d322d3d52cb6a4c0892ee2e461ab9
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-29 19:25:32 +0330

    LogOut url was added

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from core import views
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView 
 
 app_name = 'core'
 
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name="login"),
+    path('logout/', login_required(LogoutView.as_view()), name="logout"),
 ]
